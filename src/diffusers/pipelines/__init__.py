d9b875677dd727e2247546d21c54522b1bdcb3d1
victolee0 <vbnmzx1@DESKTOP-QHKOST1.localdomain>
2025-05-03 02:58:15 +0900

    add: __init__

--- src/diffusers/pipelines/__init__.py
+++ src/diffusers/pipelines/__init__.py
@@ -126,6 +126,11 @@ else:
         "AnimateDiffVideoToVideoPipeline",
         "AnimateDiffVideoToVideoControlNetPipeline",
     ]
+    _import_structure["flex"] = [
+        "Flex2Pipeline",
+        "Flex2PipelineOutput",
+    ]
+
     _import_structure["flux"] = [
         "FluxControlPipeline",
         "FluxControlInpaintPipeline",
@@ -560,6 +565,7 @@ if TYPE_CHECKING or DIFFUSERS_SLOW_IMPORT:
             EasyAnimateInpaintPipeline,
             EasyAnimatePipeline,
         )
+        from .flex import Flex2Pipeline, Flex2PipelineOutput
         from .flux import (
             FluxControlImg2ImgPipeline,
             FluxControlInpaintPipeline,
